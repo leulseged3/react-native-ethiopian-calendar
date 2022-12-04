ef86c5ece326b5ee707fccca71cca5dbe2be6bfd
leulseged tilahun <leulseged3@gmail.com>
2022-12-04 18:23:43 -0500

    chore: update example app and readme

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,41 @@
 import * as React from 'react';
 
-import { SafeAreaView, StyleSheet, ViewStyle } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, ViewStyle } from 'react-native';
 import { Calendar } from 'react-native-ethiopian-calendar';
+import type { SelectedDate } from 'src/types';
 import type { LanguageCode, Mode } from 'src/utils/locals/types';
 
 export default function App() {
   const [mode, setMode] = React.useState<Mode>('EC');
   const [locals, setLocals] = React.useState<LanguageCode>('AMH');
+  const [selectedDate, setSelectedDate] = React.useState<SelectedDate>();
 
   return (
     <SafeAreaView style={styles.container}>
       <Calendar
         mode={mode}
-        onDatePress={(date) => console.log(date)}
+        onDatePress={(date) => setSelectedDate(date)}
         onModeChange={(selectedMode) => setMode(selectedMode)}
         onLanguageChange={(lang) => setLocals(lang)}
         locale={locals}
       />
+      {
+        <View style={styles.selectedDateWrapper}>
+          <Text>Calendar Output</Text>
+          {selectedDate && (
+            <>
+              <Text>
+                Ethiopian: {selectedDate?.ethiopian.date}/
+                {selectedDate?.ethiopian.month}/{selectedDate?.ethiopian.year}
+              </Text>
+              <Text>
+                Gregorian: {selectedDate?.gregorian.date}/
+                {selectedDate?.gregorian.month}/{selectedDate?.gregorian.year}
+              </Text>
+            </>
+          )}
+        </View>
+      }
     </SafeAreaView>
   );
 }
@@ -26,4 +45,8 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 30,
   } as ViewStyle,
+  selectedDateWrapper: {
+    alignItems: 'center',
+    marginTop: 15,
+  },
 });
